d1d3d9341b5cdc478cb19970263690379733778b
Liliana Kastilio <lili@snyk.io>
2020-05-04 21:55:07 +0100

    feat: format with prettier

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,77 @@
 #!/usr/bin/env node
 
-import program = require('commander');
-import debugModule = require('debug');
-import fs = require('fs');
-import path = require('path');
-import {SnykToHtml} from './lib/snyk-to-html';
+import program = require("commander");
+import debugModule = require("debug");
+import fs = require("fs");
+import path = require("path");
+import { SnykToHtml } from "./lib/snyk-to-html";
 
 program
-.option('-t, --template <path>', 'Template location for generating the html. Defaults to template/test-report.hbs')
-.option('-i, --input <path>', 'Input path from where to read the json. Defaults to stdin')
-.option('-o, --output <path>', 'Output of the resulting HTML. Example: -o snyk.html. Defaults to stdout')
-.option('-s, --summary', 'Generates an HTML with only the summary, instead of the details report')
-.option('-d, --debug', 'Runs the CLI in debug mode')
-.option('-a, --actionable-remediation', 'Display actionable remediation info if available')
-.parse(process.argv);
+  .option(
+    "-t, --template <path>",
+    "Template location for generating the html. Defaults to template/test-report.hbs"
+  )
+  .option(
+    "-i, --input <path>",
+    "Input path from where to read the json. Defaults to stdin"
+  )
+  .option(
+    "-o, --output <path>",
+    "Output of the resulting HTML. Example: -o snyk.html. Defaults to stdout"
+  )
+  .option(
+    "-s, --summary",
+    "Generates an HTML with only the summary, instead of the details report"
+  )
+  .option("-d, --debug", "Runs the CLI in debug mode")
+  .option(
+    "-a, --actionable-remediation",
+    "Display actionable remediation info if available"
+  )
+  .parse(process.argv);
 
 let template;
 let source;
 let output;
 
-if (program.template) { // template
+if (program.template) {
+  // template
   template = program.template; // grab the next item
-  if (typeof template === 'boolean') {
-    template = path.join(__dirname, '../template/test-report.hbs');
+  if (typeof template === "boolean") {
+    template = path.join(__dirname, "../template/test-report.hbs");
   }
 } else {
-  template = path.join(__dirname, '../template/test-report.hbs');
+  template = path.join(__dirname, "../template/test-report.hbs");
 }
-if (program.input) { // input source
+if (program.input) {
+  // input source
   source = program.input; // grab the next item
-  if (typeof source === 'boolean') {
+  if (typeof source === "boolean") {
     source = undefined;
   }
 }
-if (program.output) { // output destination
+if (program.output) {
+  // output destination
   output = program.output; // grab the next item
-  if (typeof output === 'boolean') {
+  if (typeof output === "boolean") {
     output = undefined;
   }
 }
 
 if (program.debug) {
-  const nameSpace = 'snyk-to-html';
+  const nameSpace = "snyk-to-html";
   process.env.DEBUG = nameSpace;
 
   debugModule.enable(nameSpace);
 }
 
-SnykToHtml.run(source,
+SnykToHtml.run(
+  source,
   !!program.actionableRemediation,
   template,
   !!program.summary,
-  onReportOutput);
+  onReportOutput
+);
 
 function onReportOutput(report: string): void {
   if (output) {
@@ -59,7 +79,7 @@ function onReportOutput(report: string): void {
       if (err) {
         return console.log(err);
       }
-      console.log('Vulnerability snapshot saved at ' + output);
+      console.log("Vulnerability snapshot saved at " + output);
     });
   } else {
     console.log(report);
--- test/snyk-to-html.test.ts
+++ test/snyk-to-html.test.ts
@@ -1,173 +1,345 @@
-import path = require('path');
-import {test} from 'tap';
-import { SnykToHtml } from '../src/lib/snyk-to-html';
+import path = require("path");
+import { test } from "tap";
+import { SnykToHtml } from "../src/lib/snyk-to-html";
 
 const summaryOnly = true;
 const noSummary = false;
 const noRemediation = false;
 
-test('all-around test', (t) => {
+test("all-around test", t => {
   t.plan(5);
   SnykToHtml.run(
-    path.join(__dirname, 'fixtures', 'test-report.json'),
+    path.join(__dirname, "fixtures", "test-report.json"),
     noRemediation,
-    path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      noSummary,
-     (report) => {
-      t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>', 'should contain Regular Expression Denial of Service (ReDoS) vulnerability');
-      t.contains(report, '<h2 class="card__title">Cross-site Scripting (XSS)</h2>', 'should contain Cross-site Scripting (XSS) vulnerability');
-      t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (DoS)</h2>', 'should contain Regular Expression Denial of Service (DoS) vulnerability');
-      t.contains(report, '<h2 id="overview">Overview</h2>', 'should contain overview of the vulnerability');
-      t.contains(report, '<h2 id="details">Details</h2>', 'should contain description of the vulnerability');
-    });
-});
-
-test('multi-report test', (t) => {
-  t.plan(7);
-  SnykToHtml.run(
-    path.join(__dirname, 'fixtures', 'multi-test-report.json'),
-    noRemediation,
-    path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      noSummary,
-     (report) => {
-      t.contains(report, '<div class="meta-count"><span>139 vulnerable dependency paths</span></div>', 'should contain number of vulnerable dependency paths');
-      t.contains(report, '<h2 class="card__title">Access Restriction Bypass</h2>', 'should contain Access Restriction Bypass vulnerability');
-      t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>', 'should contain Regular Expression Denial of Service (ReDoS) vulnerability');
-      t.contains(report, '<h2 class="card__title">Cross-site Scripting (XSS)</h2>', 'should contain Cross-site Scripting (XSS) vulnerability');
-      t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (DoS)</h2>', 'should contain Regular Expression Denial of Service (DoS) vulnerability');
-      t.contains(report, '<h2 id="overview">Overview</h2>', 'should contain overview of the vulnerability');
-      t.contains(report, '<h2 id="details">Details</h2>', 'should contain description of the vulnerability');
-    });
-});
-
-test('multi-report test with summary only', (t) => {
-  t.plan(7);
-  SnykToHtml.run(
-      path.join(__dirname, 'fixtures', 'multi-test-report.json'),
-      noRemediation,
-      path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      summaryOnly,
-      (report) => {
-        t.contains(report, '<div class="meta-count"><span>139 vulnerable dependency paths</span></div>', 'should contain number of vulnerable dependency paths');
-        t.contains(report, '<h2 class="card__title">Access Restriction Bypass</h2>', 'should contain Access Restriction Bypass vulnerability');
-        t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>', 'should contain Regular Expression Denial of Service (ReDoS) vulnerability');
-        t.contains(report, '<h2 class="card__title">Cross-site Scripting (XSS)</h2>', 'should contain Cross-site Scripting (XSS) vulnerability');
-        t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (DoS)</h2>', 'should contain Regular Expression Denial of Service (DoS) vulnerability');
-        t.doesNotHave(report, '<h2 id="overview">Overview</h2>', 'does not contain overview of the vulnerability');
-        t.doesNotHave(report, '<h2 id="details">Details</h2>', 'does not contain details of the vulnerability');
-      });
-});
-
-test('all-around test with summary only', (t) => {
-  t.plan(5);
-  SnykToHtml.run(
-      path.join(__dirname, 'fixtures', 'test-report.json'),
-      noRemediation,
-      path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      summaryOnly,
-      (report) => {
-        t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>', 'should contain Regular Expression Denial of Service (ReDoS) vulnerability');
-        t.contains(report, '<h2 class="card__title">Cross-site Scripting (XSS)</h2>', 'should contain Cross-site Scripting (XSS) vulnerability');
-        t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (DoS)</h2>', 'should contain Regular Expression Denial of Service (DoS) vulnerability');
-        t.doesNotHave(report, '<h2 id="overview">Overview</h2>', 'does not contain overview of the vulnerability');
-        t.doesNotHave(report, '<h2 id="details">Details</h2>', 'does not contain details of the vulnerability');
-      });
-});
-
-test('all-around test with summary only with no remediation but having one fixedIn', (t) => {
-  t.plan(4);
-  SnykToHtml.run(
-      path.join(__dirname, 'fixtures', 'test-report-with-no-remediation-with-one-fixed-in.json'),
-      noRemediation,
-      path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      summaryOnly,
-      (report) => {
-        t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>', 'should contain Regular Expression Denial of Service (ReDoS) vulnerability');
-        t.contains(report, 'Fixed in: 2.9.10', 'should say: Fixed in: 2.9.10');
-        t.doesNotHave(report, '<h2 id="overview">Overview</h2>', 'does not contain overview of the vulnerability');
-        t.doesNotHave(report, '<h2 id="details">Details</h2>', 'does not contain details of the vulnerability');
-      });
-});
-
-test('all-around test with summary only with no remediation but having multiple fixedIn', (t) => {
-  t.plan(4);
-  SnykToHtml.run(
-      path.join(__dirname, 'fixtures', 'test-report-with-no-remediation-with-multiple-fixed-in.json'),
-      noRemediation,
-      path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      summaryOnly,
-      (report) => {
-        t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>', 'should contain Regular Expression Denial of Service (ReDoS) vulnerability');
-        t.contains(report, 'Fixed in: 2.9.10, 4.5.6', 'should say: Fixed in: 2.9.10, 4.5.6');
-        t.doesNotHave(report, '<h2 id="overview">Overview</h2>', 'does not contain overview of the vulnerability');
-        t.doesNotHave(report, '<h2 id="details">Details</h2>', 'does not contain details of the vulnerability');
-      });
-});
-
-test('all-around test with summary only with no remediation and no fixedIns', (t) => {
-  t.plan(4);
-  SnykToHtml.run(
-      path.join(__dirname, 'fixtures', 'test-report-with-no-remediation-and-no-fixed-in.json'),
-      noRemediation,
-      path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      summaryOnly,
-      (report) => {
-        t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>', 'should contain Regular Expression Denial of Service (ReDoS) vulnerability');
-        t.contains(report, 'There is no remediation at the moment', 'should say There is no remediation at the moment');
-        t.doesNotHave(report, '<h2 id="overview">Overview</h2>', 'does not contain overview of the vulnerability');
-        t.doesNotHave(report, '<h2 id="details">Details</h2>', 'does not contain details of the vulnerability');
-      });
-});
-
-test('empty values test (description and info)', (t) => {
-  t.plan(1);
-  SnykToHtml.run(
-      path.join(__dirname, 'fixtures', 'test-report-empty-descr.json'),
-      noRemediation,
-      path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      noSummary,
-      (report) => {
-        t.contains(report, '<p>No description available.</p>', 'should contain "No description available"');
-      });
-});
-
-test('0 vulns test (description)', (t) => {
-  t.plan(1);
-  SnykToHtml.run(
-    path.join(__dirname, 'fixtures', 'no-vulns.json'),
-    noRemediation,
-    path.join(__dirname, '..', 'template', 'test-report.hbs'),
+    path.join(__dirname, "..", "template", "test-report.hbs"),
     noSummary,
-    (report) => {
-      t.contains(report, 'No known vulnerabilities detected.', 'should contain "No known vulnerabilities detected."');
-    });
+    report => {
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>',
+        "should contain Regular Expression Denial of Service (ReDoS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Cross-site Scripting (XSS)</h2>',
+        "should contain Cross-site Scripting (XSS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (DoS)</h2>',
+        "should contain Regular Expression Denial of Service (DoS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 id="overview">Overview</h2>',
+        "should contain overview of the vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 id="details">Details</h2>',
+        "should contain description of the vulnerability"
+      );
+    }
+  );
 });
 
-test('should not generate report for invalid json', (t) => {
+test("multi-report test", t => {
+  t.plan(7);
+  SnykToHtml.run(
+    path.join(__dirname, "fixtures", "multi-test-report.json"),
+    noRemediation,
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    noSummary,
+    report => {
+      t.contains(
+        report,
+        '<div class="meta-count"><span>139 vulnerable dependency paths</span></div>',
+        "should contain number of vulnerable dependency paths"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Access Restriction Bypass</h2>',
+        "should contain Access Restriction Bypass vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>',
+        "should contain Regular Expression Denial of Service (ReDoS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Cross-site Scripting (XSS)</h2>',
+        "should contain Cross-site Scripting (XSS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (DoS)</h2>',
+        "should contain Regular Expression Denial of Service (DoS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 id="overview">Overview</h2>',
+        "should contain overview of the vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 id="details">Details</h2>',
+        "should contain description of the vulnerability"
+      );
+    }
+  );
+});
+
+test("multi-report test with summary only", t => {
+  t.plan(7);
+  SnykToHtml.run(
+    path.join(__dirname, "fixtures", "multi-test-report.json"),
+    noRemediation,
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    summaryOnly,
+    report => {
+      t.contains(
+        report,
+        '<div class="meta-count"><span>139 vulnerable dependency paths</span></div>',
+        "should contain number of vulnerable dependency paths"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Access Restriction Bypass</h2>',
+        "should contain Access Restriction Bypass vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>',
+        "should contain Regular Expression Denial of Service (ReDoS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Cross-site Scripting (XSS)</h2>',
+        "should contain Cross-site Scripting (XSS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (DoS)</h2>',
+        "should contain Regular Expression Denial of Service (DoS) vulnerability"
+      );
+      t.doesNotHave(
+        report,
+        '<h2 id="overview">Overview</h2>',
+        "does not contain overview of the vulnerability"
+      );
+      t.doesNotHave(
+        report,
+        '<h2 id="details">Details</h2>',
+        "does not contain details of the vulnerability"
+      );
+    }
+  );
+});
+
+test("all-around test with summary only", t => {
+  t.plan(5);
+  SnykToHtml.run(
+    path.join(__dirname, "fixtures", "test-report.json"),
+    noRemediation,
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    summaryOnly,
+    report => {
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>',
+        "should contain Regular Expression Denial of Service (ReDoS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Cross-site Scripting (XSS)</h2>',
+        "should contain Cross-site Scripting (XSS) vulnerability"
+      );
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (DoS)</h2>',
+        "should contain Regular Expression Denial of Service (DoS) vulnerability"
+      );
+      t.doesNotHave(
+        report,
+        '<h2 id="overview">Overview</h2>',
+        "does not contain overview of the vulnerability"
+      );
+      t.doesNotHave(
+        report,
+        '<h2 id="details">Details</h2>',
+        "does not contain details of the vulnerability"
+      );
+    }
+  );
+});
+
+test("all-around test with summary only with no remediation but having one fixedIn", t => {
+  t.plan(4);
+  SnykToHtml.run(
+    path.join(
+      __dirname,
+      "fixtures",
+      "test-report-with-no-remediation-with-one-fixed-in.json"
+    ),
+    noRemediation,
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    summaryOnly,
+    report => {
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>',
+        "should contain Regular Expression Denial of Service (ReDoS) vulnerability"
+      );
+      t.contains(report, "Fixed in: 2.9.10", "should say: Fixed in: 2.9.10");
+      t.doesNotHave(
+        report,
+        '<h2 id="overview">Overview</h2>',
+        "does not contain overview of the vulnerability"
+      );
+      t.doesNotHave(
+        report,
+        '<h2 id="details">Details</h2>',
+        "does not contain details of the vulnerability"
+      );
+    }
+  );
+});
+
+test("all-around test with summary only with no remediation but having multiple fixedIn", t => {
+  t.plan(4);
+  SnykToHtml.run(
+    path.join(
+      __dirname,
+      "fixtures",
+      "test-report-with-no-remediation-with-multiple-fixed-in.json"
+    ),
+    noRemediation,
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    summaryOnly,
+    report => {
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>',
+        "should contain Regular Expression Denial of Service (ReDoS) vulnerability"
+      );
+      t.contains(
+        report,
+        "Fixed in: 2.9.10, 4.5.6",
+        "should say: Fixed in: 2.9.10, 4.5.6"
+      );
+      t.doesNotHave(
+        report,
+        '<h2 id="overview">Overview</h2>',
+        "does not contain overview of the vulnerability"
+      );
+      t.doesNotHave(
+        report,
+        '<h2 id="details">Details</h2>',
+        "does not contain details of the vulnerability"
+      );
+    }
+  );
+});
+
+test("all-around test with summary only with no remediation and no fixedIns", t => {
+  t.plan(4);
+  SnykToHtml.run(
+    path.join(
+      __dirname,
+      "fixtures",
+      "test-report-with-no-remediation-and-no-fixed-in.json"
+    ),
+    noRemediation,
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    summaryOnly,
+    report => {
+      t.contains(
+        report,
+        '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)</h2>',
+        "should contain Regular Expression Denial of Service (ReDoS) vulnerability"
+      );
+      t.contains(
+        report,
+        "There is no remediation at the moment",
+        "should say There is no remediation at the moment"
+      );
+      t.doesNotHave(
+        report,
+        '<h2 id="overview">Overview</h2>',
+        "does not contain overview of the vulnerability"
+      );
+      t.doesNotHave(
+        report,
+        '<h2 id="details">Details</h2>',
+        "does not contain details of the vulnerability"
+      );
+    }
+  );
+});
+
+test("empty values test (description and info)", t => {
+  t.plan(1);
+  SnykToHtml.run(
+    path.join(__dirname, "fixtures", "test-report-empty-descr.json"),
+    noRemediation,
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    noSummary,
+    report => {
+      t.contains(
+        report,
+        "<p>No description available.</p>",
+        'should contain "No description available"'
+      );
+    }
+  );
+});
+
+test("0 vulns test (description)", t => {
+  t.plan(1);
+  SnykToHtml.run(
+    path.join(__dirname, "fixtures", "no-vulns.json"),
+    noRemediation,
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    noSummary,
+    report => {
+      t.contains(
+        report,
+        "No known vulnerabilities detected.",
+        'should contain "No known vulnerabilities detected."'
+      );
+    }
+  );
+});
+
+test("should not generate report for invalid json", t => {
   t.plan(0);
   SnykToHtml.run(
-      path.join(__dirname, 'fixtures', 'invalid-input.json'),
-      noRemediation,
-      path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      noSummary,
-      (report) => {
-        t.match(report, '', 'report object is empty');
-      });
+    path.join(__dirname, "fixtures", "invalid-input.json"),
+    noRemediation,
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    noSummary,
+    report => {
+      t.match(report, "", "report object is empty");
+    }
+  );
 });
 
-test('template output displays vulns in descending order of severity ', (t) => {
+test("template output displays vulns in descending order of severity ", t => {
   SnykToHtml.run(
-    path.join(__dirname, 'fixtures', 'multi-test-report.json'),
+    path.join(__dirname, "fixtures", "multi-test-report.json"),
     noRemediation,
-    path.join(__dirname, '..', 'template', 'test-report.hbs'),
-      summaryOnly,
-      (report) => {
-        const regex = /<p class="timestamp">.*<\/p>/g;
-        const cleanTimestamp = rep => rep.replace(regex, '<p class="timestamp">TIMESTAMP</p>');
-        const cleanedReport = cleanTimestamp(report);
-        // compares against snapshot in tap-snapshots/test-snyk-to-html.test.ts-TAP.test.js
-        // to re-generate snapshots: tap test.js --snapshot
-        t.matchSnapshot(cleanedReport, 'should be expected snapshot');
-        t.end();
-      });
+    path.join(__dirname, "..", "template", "test-report.hbs"),
+    summaryOnly,
+    report => {
+      const regex = /<p class="timestamp">.*<\/p>/g;
+      const cleanTimestamp = rep =>
+        rep.replace(regex, '<p class="timestamp">TIMESTAMP</p>');
+      const cleanedReport = cleanTimestamp(report);
+      // compares against snapshot in tap-snapshots/test-snyk-to-html.test.ts-TAP.test.js
+      // to re-generate snapshots: tap test.js --snapshot
+      t.matchSnapshot(cleanedReport, "should be expected snapshot");
+      t.end();
+    }
+  );
 });
